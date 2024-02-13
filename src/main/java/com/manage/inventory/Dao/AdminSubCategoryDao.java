c363e2bd3a8bde7b92f9f16a474e30c71492a83c
achallion <bhavyarocks19@gmail.com>
2024-02-14 02:56:18 +0530

    add subcategory dao for admin
    Signed-off-by: achallion <bhavyarocks19@gmail.com>

--- src/main/java/com/manage/inventory/Dao/AdminSubCategoryDao.java
+++ src/main/java/com/manage/inventory/Dao/AdminSubCategoryDao.java
@@ -1,11 +1,18 @@
 package com.manage.inventory.Dao;
 
+import java.util.Optional;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.manage.inventory.Model.SubCategory;
 
 @Repository
 public interface AdminSubCategoryDao extends JpaRepository<SubCategory, Integer> {
+
+	@Query("SELECT subCategory FROM SubCategory subCategory where subCategory.name = ?1")
+	Optional<SubCategory> findByName(String name);
 
 }
