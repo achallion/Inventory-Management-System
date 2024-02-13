b8d9f1588e88ff1e70bd863c3c437598c43f3fa2
achallion <bhavyarocks19@gmail.com>
2024-02-14 02:55:33 +0530

    add product dao for admin
    Signed-off-by: achallion <bhavyarocks19@gmail.com>

--- src/main/java/com/manage/inventory/Dao/AdminProductDao.java
+++ src/main/java/com/manage/inventory/Dao/AdminProductDao.java
@@ -1,6 +1,9 @@
 package com.manage.inventory.Dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.manage.inventory.Model.Product;
@@ -8,4 +11,6 @@ import com.manage.inventory.Model.Product;
 @Repository
 public interface AdminProductDao extends JpaRepository<Product, Integer> {
 
+	@Query("SELECT product FROM Product product WHERE product.name = ?1")
+	Optional<Product> findByName(String name);
 }
